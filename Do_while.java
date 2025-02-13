2db6dd7f5d6dee1dea33d04d09d5a582c23b3719
Ulvi-2707 <ulvi2707@gmail.com>
2025-02-13 17:44:47 +0400

    Add files via upload

---
+++ Do_while.java
@@ -0,0 +1,87 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Do_while {
+    public static void main(String[] args) {
+
+//        Məsələ 1:
+//        İstifadəçidən bir şifrə daxil etməsini istəyin. Şifrə doğru daxil edilənə qədər
+//        bu prosesi təkrarlayın. Şifrə düzgün olduqda "Giriş uğurla tamamlandı" mesajını yazdırın.
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        String correctPassword = "123++aa";
+//        String inputPassword;
+//        do {
+//            System.out.println("Enter your password: ");
+//            inputPassword = sc.nextLine();
+//            if (inputPassword.equals(correctPassword)) {
+//                System.out.println("Successful login" );
+//            }
+//            else {
+//                System.err.println("Wrong password");
+//            }
+//
+//        } while (!inputPassword.equals(correctPassword));
+
+//        Mesele 2
+//       İstifadəçidən bir ədəd daxil etməsini istəyin və bu ədədin kvadratını hesablamağa davam edin,
+//       amma daxil edilən ədəd 0 olduqda dövrə dayansın.
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        int num;
+//        do {
+//            System.out.print("Enter number: ");
+//            num = sc.nextInt();
+//            if (num != 0) {
+//                int sqr = num * num;
+//                System.out.println("Square of number " + sqr);
+//            }
+//        }
+//        while (num !=0);
+//        System.err.println("End of the calculation");
+
+
+//        Məsələ 3:
+//        İstifadəçidən ardıcıl olaraq ədədlər alın və bunların daxilindəki ən böyük ədədi tapın.
+//        İstifadəçi 0 daxil etdikdə dövrə dayansın.
+
+//        Scanner sc = new Scanner(System.in);
+//        int num;
+//        int maxNumber = Integer.MIN_VALUE;
+//        do {
+//            System.out.print("Enter number or 0 to exit: ");
+//            num = sc.nextInt();
+//            if (num != 0 && num > maxNumber) {
+//                maxNumber = num;
+//            }
+//
+//        }
+//        while (num != 0);
+//        System.out.print("Max number is: " + maxNumber);
+
+
+        //    Məsələ 4:
+//    Bir neçə ədəd daxil edin və bu ədədlərdən hansının ən kiçik olduğunu tapın.
+//    İstifadəçi sıfır daxil etdikdə dövrə dayansın.
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int num;
+//        int minNumber= Integer.MAX_VALUE;
+//        do {
+//            System.out.print("Enter number or 0 to exit: ");
+//            num = sc.nextInt();
+//            if (num!=0 && num < minNumber) {
+//                minNumber = num;
+//            }
+//        }
+//        while (num!=0);
+//        System.out.println("Min number is: " + minNumber);
+
+    }
+
+
+}
